8117279862c81d0427ca32aeee0b2a4e1f489334
bitcoinark <chris@bitcoinark.org>
2025-02-26 12:46:07 +1100

    Update scheduler.py

--- src/scheduler.py
+++ src/scheduler.py
@@ -1,1 +1,14 @@
+import time
 
+class Scheduler:
+    def __init__(self, blockchain, notifier, cadence_seconds):
+        self.blockchain = blockchain
+        self.notifier = notifier
+        self.cadence_seconds = cadence_seconds
+
+    def run(self):
+        """Run the monitoring loop indefinitely."""
+        while True:
+            if self.blockchain.check_utxo():
+                self.notifier.notify(self.blockchain.txid, self.blockchain.vout)
+            time.sleep(self.cadence_seconds)
