20b83a03e111616e8432b57429b2590ae6fbedc5
bitcoinark <chris@bitcoinark.org>
2025-02-26 13:06:06 +1100

    Update scheduler.py
    Added debug logging for each cycle.

--- src/scheduler.py
+++ src/scheduler.py
@@ -1,14 +1,17 @@
 import time
 
 class Scheduler:
-    def __init__(self, blockchain, notifier, cadence_seconds):
+    def __init__(self, blockchain, notifier, cadence_seconds, logger):
         self.blockchain = blockchain
         self.notifier = notifier
         self.cadence_seconds = cadence_seconds
+        self.logger = logger
 
     def run(self):
         """Run the monitoring loop indefinitely."""
         while True:
+            self.logger.debug("Checking UTXO status")
             if self.blockchain.check_utxo():
                 self.notifier.notify(self.blockchain.txid, self.blockchain.vout)
+            self.logger.debug(f"Sleeping for {self.cadence_seconds} seconds")
             time.sleep(self.cadence_seconds)
